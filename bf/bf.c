a36d6757ddea28fa5418bad0dd2eee6bbced444d
judy9710 <judy9710@naver.com>
2023-12-21 16:04:33 +0900

    added bf.c

---
+++ bf/bf.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "minirel.h"
+#include "bf.h"
+
+typedef struct BFpage{
+  PFpage         fpage;       /* page data from the file                 */
+  struct BFpage  *nextpage;   /* next in the linked list of buffer pages */
+  struct BFpage  *prevpage;   /* prev in the linked list of buffer pages */
+  bool_t         dirty;       /* TRUE if page is dirty                   */
+  short          count;       /* pin count associated with the page      */
+  int            pageNum;     /* page number of this page                */
+  int            fd;          /* PF file descriptor of this page         */
+  int            unixfd;      /* Unix file descriptor of this page       */
+} BFpage;
+
+typedef struct BFhash_entry {
+  struct BFhash_entry *nextentry;     /* next hash table element or NULL */
+  struct BFhash_entry *preventry;     /* prev hash table element or NULL */
+  int fd;                             /* file descriptor                 */
+  int pageNum;                        /* page number                     */
+  struct BFpage *bpage;               /* ptr to buffer holding this page */
+} BFhash_entry;
+
+
+void BF_Init(void){
+    BFpage* bfentryhead = (BFpage*)malloc(sizeof(BFpage)); /* creates the buffer entry */ 
+    BFpage* freelisthead; /* initiate the free list */ 
+    freelisthead.nextpage = &bfentryhead; /* add the buffer entry to the free list */
+    struct BFhash_entry hashtablehead; /* initializes the hash table */
+}
